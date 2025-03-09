1e2605066e4cbf82e01fead434df50912d0848e6
Kreisi-Paul <111511210+Kreisi-Paul@users.noreply.github.com>
2025-03-09 02:20:35 +0100

    undid faulty property management

--- src/BezierCurve2.js
+++ src/BezierCurve2.js
@@ -2,43 +2,47 @@
 
 class BezierCurve2 {
     /** @type {Point2} @private */
+    #p0;
     get p0() {
-        return this.p0
+        return this.#p0
     }
     set p0(point) {
         if(Object.getPrototypeOf(point) != Point2.prototype)
             throw new TypeError("Invalid object passed");
-        this.p0 = point;
+        this.#p0 = point;
     }
 
     /** @type {Point2} @private */
+    #p1;
     get p1() {
-        return this.p1
+        return this.#p1
     }
     set p1(point) {
         if(Object.getPrototypeOf(point) != Point2.prototype)
             throw new TypeError("Invalid object passed");
-        this.p1 = point;
+        this.#p1 = point;
     }
 
     /** @type {Point2} @private */
+    #p2;
     get p2() {
-        return this.p2
+        return this.#p2
     }
     set p2(point) {
         if(Object.getPrototypeOf(point) != Point2.prototype)
             throw new TypeError("Invalid object passed");
-        this.p2 = point;
+        this.#p2 = point;
     }
 
     /** @type {Number} @private */
+    #intermediates;
     get intermediates() {
-        return this.intermediates;
+        return this.#intermediates;
     }
     set intermediates(amount) {
         if(typeof amount != "number" && amount%1==0)
             throw new TypeError("Invalid object passed");
-        this.intermediates = amount;
+        this.#intermediates = amount;
     }
 
     /**
@@ -48,10 +52,10 @@ class BezierCurve2 {
      * @param {Number} intermediates 
      */
     constructor(p0, p1, p2, intermediates) {
-        this.p0 = p0 || new Point2(0, 0);
-        this.p1 = p1 || new Point2(0, 0);
-        this.p2 = p2 || new Point2(0, 0);
-        this.intermediates = intermediates || 10;
+        this.#p0 = p0 || new Point2(0, 0);
+        this.#p1 = p1 || new Point2(0, 0);
+        this.#p2 = p2 || new Point2(0, 0);
+        this.#intermediates = intermediates || 10;
     }
 
     /**
@@ -79,15 +83,15 @@ class BezierCurve2 {
         /** intermediates between p1 and p2 @type {Point2[]} */
         let q1 = new Array();
 
-        let l0 = new Line2(this.p0, this.p1);
-        let l1 = new Line2(this.p1, this.p2);
+        let l0 = new Line2(this.#p0, this.#p1);
+        let l1 = new Line2(this.#p1, this.#p2);
 
-        q0 = l0.getIntermediates(this.intermediates, true);
-        q1 = l1.getIntermediates(this.intermediates, true);
+        q0 = l0.getIntermediates(this.#intermediates, true);
+        q1 = l1.getIntermediates(this.#intermediates, true);
 
-        for(let i=0; i<=this.intermediates; i++) {
+        for(let i=0; i<=this.#intermediates; i++) {
             let tmpVector = new Line2(q0[i], q1[i]).getVector();
-            tmpVector.multiplyMagnitude((1/this.intermediates)*i);
+            tmpVector.multiplyMagnitude((1/this.#intermediates)*i);
 
             q0[i].applyVector(tmpVector);
 
