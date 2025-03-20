e13ed8fbad45229adaac71dbe57d23559b569693
Kreisi-Paul <111511210+Kreisi-Paul@users.noreply.github.com>
2025-03-20 23:36:52 +0100

    adjusted poc.js to classname changes

--- test/poc.js
+++ test/poc.js
@@ -1,7 +1,7 @@
 const Point2 = require("../out/cjs/Point2");
-const BezierCurve2 = require("../out/cjs/BezierCurve2");
+const QuadraticBezierCurve2 = require("../out/cjs/QuadraticBezierCurve2");
 
-let tmp = new BezierCurve2(
+let tmp = new QuadraticBezierCurve2(
     new Point2(100, 100),
     new Point2(800, 100),
     new Point2(800, 800),
